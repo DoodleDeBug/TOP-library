99271ff8baf451068f9c23bc1df2c2a534376f25
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-06-02 20:13:01 +0100

    create book object construvtor

---
+++ index.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(name, author, pages, status) {
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.status = status
+    // the constructor...
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
